a3bb888f4a1bfd46dff7a56dcf7b85ae6720f660
Muthu511 <37763782+Muthu511@users.noreply.github.com>
2019-09-18 20:55:23 +0530

    Add files via upload

---
+++ admin/includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>All rights reserved</b>
+    </div>
+    <strong>Copyright &copy;2019 Brought To You By<a href="https://code-projects.org/">Diverta Bazar</a></strong>
+</footer>
